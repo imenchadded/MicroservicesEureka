a8d4a473cddeea983212d47877dfdfc5e5c1b038
Imen Chadded <imen.chadded@bcs.local>
2024-03-09 11:29:13 +0100

    commit config-server

---
+++ src/main/java/com/imen/configserver/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.imen.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
+
+
+
+//ajout de model matier ou prof apatrir de pom.xml
